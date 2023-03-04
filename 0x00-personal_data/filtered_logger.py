b9b24fa57614015e0c7b222609f979fcec0951fe
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-03-04 07:42:11 +0000

    added documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,17 @@ def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
                  separator: str) -> str:
+    """
+    filter-datum: returns log message obfuscated
+    Args:
+        fields (List): list of strings representing all fields
+        redaction (str): string representing field that will be
+            obfuscated
+        message (str): string representing the log line
+        separator: string representing character to separate fields
+    Returns:
+        Log message obfuscated
+    """
     for m in fields:
         message = re.sub(f'{m}=.*?{separator}',
                          f'{m}={redaction}{separator}',

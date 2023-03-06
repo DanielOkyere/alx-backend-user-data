e7c6f67b308966222ab3082cc926163758ffe5d3
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-03-06 22:48:25 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,20 @@
 """Class basic Auth"""
 
 from .auth import Auth
+import base64
+import re
 
 
 class BasicAuth(Auth):
     """Basic Auth inherits Auth"""
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """Extract Base64 authorization"""
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            f_match = re.fullmatch(pattern, authorization_header.strip())
+            if f_match is not None:
+                return f_match.group('token')
+        return None

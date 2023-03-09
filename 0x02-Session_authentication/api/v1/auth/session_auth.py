7b52c30caeec78c0040ff67a1830f88cccfddeb8
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-03-09 14:37:22 +0000

    done create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """Empty Session"""
-from .auth import Auth
+from auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session Auth that inherits Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session Id"""
+        if not isinstance(user_id, str)\
+           or user_id is None:
+            return None
+        new_key = str(uuid4())
+        self. user_id_by_session_id[new_key] = user_id
+        return new_key
